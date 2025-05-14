362d20c54ef4c7af848791c6c40606bbe4173ff2
RathlavathAnusha411 <22d21a05n8anusha@gmail.com>
2025-05-14 11:19:46 +0530

    Add files via upload
    moving 0 with the end of the list

---
+++ movingeven.py
@@ -0,0 +1,17 @@
+li=[2,3,6,7,8,1,5]
+n=len(li)
+k=0
+for i in range(n):
+    if li[i]%2==0:
+
+print(li)        
+#
+
+l=[2,3,6,7,8,1,5] # indexes 0 1 2 3 4 5
+k=0
+for i in range(len(l)):
+    if l[i]%2!:=0:
+    temp=l.pop(i)
+    l.insert(k,temp)
+    k+=1
+print(l)
